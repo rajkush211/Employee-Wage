cd1bba9859d43b4fe82dbfcf3c22127fcc2f71dc
rajkush211 <rajkush211.rk@gmail.com>
2020-03-12 14:16:43 +0530

    ADDED: UC1 Checking Employee is Present or Not

---
+++ EmployeeWage.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -x
+
+clear
+
+isPresent=1;
+randomCheck=$(( RANDOM % 2 ));
+
+if [ $isPresent -eq $randomCheck ];
+then
+        echo "Employee is Present";
+else
+        echo "Employee is Absent";
+fi
+
+sleep 2
